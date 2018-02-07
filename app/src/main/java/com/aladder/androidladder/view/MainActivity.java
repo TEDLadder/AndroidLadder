d975f7b7dfe73acb75b062e4009138bb4e2cc36d
syzhugh <syzhugh@163.com>
2018-02-07 17:20:54 +0800

    code init

---
+++ app/src/main/java/com/aladder/androidladder/view/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aladder.androidladder.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.aladder.androidladder.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
