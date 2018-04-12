e5b114ebf348b9d3ef343105f93887eb69ff5c27
syzhugh <syzhugh@163.com>
2018-04-12 23:40:04 +0800

    view - 收起展开的textview

--- app/src/main/java/com/aladder/androidladder/MainActivity.java
+++ app/src/main/java/com/aladder/androidladder/MainActivity.java
@@ -1,8 +1,10 @@
-package com.aladder.androidladder.view;
+package com.aladder.androidladder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import com.aladder.androidladder.R;
+import com.aladder.androidladder.view.text.ExpandTextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ExpandTextView expandTextView = (ExpandTextView) findViewById(R.id.main_text);
+        expandTextView.setText("111\n222\n333\n444\n555\n666\n777\n");
+        expandTextView.setMaxLine(5);
     }
 }
