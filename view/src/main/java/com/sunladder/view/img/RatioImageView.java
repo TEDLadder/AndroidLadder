0f19db45c0557e6566c4f8cbf1e31e671e3d1483
syzhugh <syzhugh@163.com>
2018-05-27 20:53:48 +0800

    view - ratioImageView

---
+++ view/src/main/java/com/sunladder/view/img/RatioImageView.java
@@ -0,0 +1,45 @@
+package com.sunladder.view.img;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+
+/**
+ * An ImageView whose height depends on its width and the ratio.
+ */
+
+public class RatioImageView extends AppCompatImageView {
+
+    private float mRatio = 1.0F;
+
+    public RatioImageView(Context context) {
+        this(context, null);
+    }
+
+    public RatioImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public RatioImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void setRatio(float ratio) {
+        if (ratio > 0 && ratio != mRatio) {
+            mRatio = ratio;
+            requestLayout();
+            invalidate();
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int measuredWidth = getMeasuredWidth();
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        if (heightMode != MeasureSpec.EXACTLY) {
+            setMeasuredDimension(measuredWidth, (int) (measuredWidth * mRatio));
+        }
+    }
+}
