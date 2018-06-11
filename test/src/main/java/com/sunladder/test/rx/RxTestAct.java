091e292070d99a05dae57c665bbe16cb8d85dcef
syzhugh <syzhugh@163.com>
2018-06-11 08:58:08 +0800

    test - rxjava call chain

--- test/src/main/java/com/sunladder/test/rx/RxTestAct.java
+++ test/src/main/java/com/sunladder/test/rx/RxTestAct.java
@@ -6,9 +6,11 @@ import com.sunladder.common.log.Logger;
 import com.sunladder.test.R;
 import rx.Observable;
 import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.observables.SyncOnSubscribe;
+import rx.schedulers.Schedulers;
 
 public class RxTestAct extends AppCompatActivity {
 
@@ -19,20 +21,82 @@ public class RxTestAct extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx_test);
 
-        roundTwo();
+//        roundOne_producer();
+        roundTwo_callChain();
+//        roundThree_scheduler();
     }
 
-    private void roundTwo() {
+    // TODO: 2018/6/11  如何进行线程调度
+    private void roundThree_scheduler() {
         Observable.just(0)
+                .map(new Func1<Integer, Integer>() {
+                    @Override
+                    public Integer call(Integer integer) {
+                        return ++integer;
+                    }
+                })
+                .subscribeOn(Schedulers.newThread())
                 .flatMap(new Func1<Integer, Observable<?>>() {
                     @Override
                     public Observable<Integer> call(Integer integer) {
-                        return Observable.just(integer);
+                        return Observable.just(++integer)
+                                .subscribeOn(Schedulers.newThread());
                     }
                 })
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Action1<Object>() {
                     @Override
                     public void call(Object o) {
+                    }
+                });
+    }
+
+    // TODO: 2018/6/11  如何形成调用链
+    private void roundTwo_callChain() {
+        Observable<Integer> observable1 = Observable.just(0);
+        Logger.printVar("observable1", observable1);
+        Observable<Integer> observable2 = observable1
+                .map(new Func1<Integer, Integer>() {
+                    @Override
+                    public Integer call(Integer integer) {
+                        String tag = "step1";
+                        Logger.printCurrentThread(tag);
+                        Logger.printVar(tag, integer);
+                        return ++integer;
+                    }
+                });
+        Logger.printVar("observable2", observable2 == observable1);
+        Observable<Integer> observable3 = observable2
+                .subscribeOn(Schedulers.newThread());
+        Logger.printVar("observable3", observable3 == observable2);
+        Observable<Integer> observable4 = observable3
+                .map(new Func1<Integer, Integer>() {
+                    @Override
+                    public Integer call(Integer integer) {
+                        String tag = "step2";
+                        Logger.printCurrentThread(tag);
+                        Logger.printVar(tag, integer);
+                        return ++integer;
+                    }
+                });
+        Logger.printVar("observable4", observable4 == observable3);
+        Observable<Integer> observable5 = observable4.subscribeOn(Schedulers.newThread());
+        Logger.printVar("observable5", observable5 == observable4);
+        observable5
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer integer) {
+                        String tag = "finish";
+                        Logger.printCurrentThread(tag);
+                        Logger.printVar(tag, integer);
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+
                     }
                 });
     }
@@ -41,7 +105,7 @@ public class RxTestAct extends AppCompatActivity {
     /**
      * create&just producer差异
      */
-    private void roundOne() {
+    private void roundOne_producer() {
         normal();
         testCreate();
     }
