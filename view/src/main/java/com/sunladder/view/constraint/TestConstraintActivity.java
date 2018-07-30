c4a93641527bb7ad2d3b7c847ac397ff0d8381f3
syzhugh <syzhugh@163.com>
2018-07-30 11:50:08 +0800

    view - constraintLayout

---
+++ view/src/main/java/com/sunladder/view/constraint/TestConstraintActivity.java
@@ -0,0 +1,14 @@
+package com.sunladder.view.constraint;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.sunladder.view.R;
+
+public class TestConstraintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_activity_test_constraint);
+    }
+}
