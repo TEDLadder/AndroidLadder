33b3b68d10097a9fbf3f613db1fc180d8081ccdb
syzhugh <syzhugh@163.com>
2018-10-01 11:24:27 +0800

    temp

---
+++ kotlin/src/main/kotlin/com/kotlinladder/sample/TestView.java
@@ -0,0 +1,27 @@
+package com.kotlinladder.sample;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Sun on 2018/9/15.
+ */
+
+public class TestView extends View{
+    private Paint mPaint;
+
+    public TestView(Context context) {
+        this(context, null);
+    }
+
+    public TestView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TestView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
